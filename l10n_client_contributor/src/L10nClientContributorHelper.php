b408c29e429eefaeeffecf997270b269d455cf20
azhardotcoder <azhar.khan358113@gmail.com>
2024-08-14 16:40:58 +0530

    implemented basic auth

--- l10n_client_contributor/src/L10nClientContributorHelper.php
+++ l10n_client_contributor/src/L10nClientContributorHelper.php
@@ -8,11 +8,10 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\Core\Logger\LoggerChannelInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\oauth2_client\Service\Oauth2ClientServiceInterface;
 use Psr\Http\Message\ResponseInterface;
 
 /**
- * @todo Add class description.
+ * Provides helper services for the Localization Client Contributor module.
  */
 final class L10nClientContributorHelper implements L10nClientContributorHelperInterface {
 
@@ -33,7 +32,6 @@ final class L10nClientContributorHelper implements L10nClientContributorHelperIn
   public function __construct(
     ConfigFactoryInterface $config_factory,
     LoggerChannelFactory $logger_factory,
-    protected readonly Oauth2ClientServiceInterface $oauth2ClientService,
     protected readonly ClientInterface $httpClient,
   ) {
     $this->config = $config_factory->get('l10n_client_contributor.settings');
@@ -41,62 +39,66 @@ final class L10nClientContributorHelper implements L10nClientContributorHelperIn
   }
 
   /**
-   * {@inheritdoc}
+   * Sends translation to the localization server using basic authentication.
+   *
+   * @param string $langcode
+   *   The language code of a translated string.
+   * @param string $source
+   *   The translatable string.
+   * @param string $translation
+   *   The translated string.
+   * @param string $context
+   *   The context of the source string.
+   *
+   * @return array
+   *   The result of the send request.
+   *   [response code/FALSE, message]
    */
-  public function isAuthenticated(): bool {
-    $client = $this->oauth2ClientService->getClient('l10n_authcode');
-    return (bool) $client->retrieveAccessToken();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getAccessToken(): ?string {
-    return (string) $this->oauth2ClientService->getAccessToken('l10n_authcode', NULL);
-  }
-
   public function sendTranslation($langcode, $source, $translation, $context = ''): array {
-    if (!$oauth_token = $this->getAccessToken()) {
-      $message = $this->t('Failed to get access token.');
-      $this->logger->error($message);
-      return [FALSE, $message];
-    }
-    
     $server_url = $this->config->get('server');
+    $username = $this->config->get('basic_auth_user');
+    $password = $this->config->get('basic_auth_pass');
+
     try {
-    $response = $this->httpClient->post($server_url . 'api/l10n-server-contributor', [
-      'headers' => [
-        'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer ' . $oauth_token,
-      ],
-      'json' => [
-        'source' => $source,
-        'translation' => $translation,
-        'langcode' => $langcode,
-        'context' => $context,
-      ],
-    ]);
+      $response = $this->httpClient->post($server_url . 'api/l10n-server-contributor', [
+        'headers' => [
+          'Content-Type' => 'application/json',
+          'Authorization' => 'Basic ' . base64_encode("$username:$password"),
+        ],
+        'json' => [
+          'source' => $source,
+          'translation' => $translation,
+          'langcode' => $langcode,
+          'context' => $context,
+        ],
+      ]);
     }
     catch (\Exception $e) {
       $message = $e->getMessage();
       $this->logger->error($message);
       return [FALSE, $message];
     }
-  
+
     if ($response instanceof ResponseInterface) {
       if ($response->getStatusCode() == 201) {
-        $message = $this->t('Translation sent and accepted by @server.', array('@server' => $server_url));
-        $this->logger->notice('Translation sent and accepted by @server.', array('@server' => $server_url));
+        $message = $this->t('Translation sent and accepted by @server.', ['@server' => $server_url]);
+        $this->logger->notice('Translation sent and accepted by @server.', ['@server' => $server_url]);
       }
       else {
-        $message = $this->t('Translation rejected by @server. Reason: %reason', array('%reason' => $response->getReasonPhrase(), '@server' => $server_url));
-        $this->logger->error('Translation rejected by @server. Reason: %reason', array('%reason' => $response->getReasonPhrase(), '@server' => $server_url));
+        $message = $this->t('Translation rejected by @server. Reason: %reason', [
+          '%reason' => $response->getReasonPhrase(),
+          '@server' => $server_url
+        ]);
+        $this->logger->error('Translation rejected by @server. Reason: %reason', [
+          '%reason' => $response->getReasonPhrase(),
+          '@server' => $server_url
+        ]);
       }
-      return array($response->getStatusCode(), $message);
+      return [$response->getStatusCode(), $message];
     }
     else {
-      $message = $this->t('The connection with @server failed.');
-      return array(FALSE, $message);
+      $message = $this->t('The connection with @server failed.', ['@server' => $server_url]);
+      return [FALSE, $message];
     }
   }
 
--- l10n_client_contributor/src/L10nClientContributorHelperInterface.php
+++ l10n_client_contributor/src/L10nClientContributorHelperInterface.php
@@ -3,42 +3,26 @@
 namespace Drupal\l10n_client_contributor;
 
 /**
- * @todo Add interface description.
+ * Provides an interface for Localization Client Contributor helper service.
  */
 interface L10nClientContributorHelperInterface {
 
   /**
-   * Check whether the current user already has a bearer token.
+   * Sends translation to the server.
    *
-   * @return boolean
-   *   TRUE, if it is autheticated.
-   */
-  public function isAuthenticated(): bool;
-
-  /**
-   * Get the bearer token.
+   * @param string $langcode
+   *   The language code of a translated string.
+   * @param string $source
+   *   The translatable string.
+   * @param string $translation
+   *   The translated string.
+   * @param string $context
+   *   The context of the source string.
    *
-   * @return string|null
-   *   The bearer token
+   * @return array
+   *   The result of the send request.
+   *   [response code/FALSE, message]
    */
-  public function getAccessToken(): ?string;
-
-  /**
- * Send translation to the server.
- *
- * @param string $langcode
- *   The language code of a translated string.
- * @param string $source
- *   The translatable string.
- * @param string $translation
- *   The translated string.
- * @param string $context.
- *   The context of the source string.
- *
- * @return array
- *    The result of the send request.
- *    [response code/FALSE, message]
- */
   public function sendTranslation($langcode, $source, $translation, $context = ''): array;
 
 }
